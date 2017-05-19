eb61f0591ddcd9e3faf8684bccd142c226614597
shanm <shanmugaraj.kumar@gmail.com>
2017-05-19 17:35:07 +0530

    Added new css and js script

---
+++ angular/angular-master/js/js.js
@@ -0,0 +1,12 @@
+/*function helloworld() {
+var apple = {"color":"red", "width":30, "height": 40};
+console.log(apple);
+}
+helloworld();
+
+var apple2 = {"color":"red", "width":15, "height": 20};
+console.log(apple2);
+*/
+$(document).ready(function() {
+	console.log(Welcome user!!!!);
+)};
